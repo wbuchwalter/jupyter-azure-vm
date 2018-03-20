de86d1e3aa08d702d07793c1dbc8f681a44ef038
William Buchwalter <wbuchwalter@gmail.com>
2018-03-20 10:13:20 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ regUsername=$3
 regPassword=$4
 
 sudo docker login $server -u $regUsername -p $regPassword
-sudo nvidia-docker run -d -p 5555:5555 -p 80:7842 -p 8787:8787 -p 8786:8786 -p 8788:8788 -v ~/dev/:/root/sharedfolder $image sh ./run_jupyter.sh
+sudo nvidia-docker run -d -p 80:8888 $image
