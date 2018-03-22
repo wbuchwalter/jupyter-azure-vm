20bf39e53b3c57cfbb9ad2f2def0cf153a19ed3d
William Buchwalter <wbuchwalter@gmail.com>
2018-03-22 15:48:53 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,13 @@ image=$2
 regUsername=$3
 regPassword=$4
 
-echo "DEBUG 0: $0"
-echo "DEBUG 1: $1"
-echo "DEBUG 2: $2"
-echo "DEBUG 3: $3"
-echo "DEBUG 4: $4"
-
 echo "DEBUG: sudo docker login $server -u $regUsername -p $regPassword && sudo nvidia-docker run -d -p 80:8888 $image"
-sudo docker login $server -u $regUsername -p $regPassword && sudo nvidia-docker run -d -p 80:8888 $image
+n=0
+until [ $n -ge 5 ]
+do
+  sudo docker login $server -u $regUsername -p $regPassword && sudo nvidia-docker run -d -p 80:8888 $image && break 
+  echo "Failed to start the docker image. Will retry in 15 seconds"
+  n=$[$n+1]
+  sleep 15
+done
+
