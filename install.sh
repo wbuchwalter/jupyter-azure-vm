2c4c90924ca8b48858ce867a8e599e7c8ec8f51f
William Buchwalter <wbuchwalter@gmail.com>
2018-03-22 14:40:22 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,12 @@ server=$1
 image=$2
 regUsername=$3
 regPassword=$4
+
+echo "DEBUG 0: $0"
+echo "DEBUG 1: $1"
+echo "DEBUG 2: $2"
+echo "DEBUG 3: $3"
+echo "DEBUG 4: $4"
+
 echo "DEBUG: sudo docker login $server -u $regUsername -p $regPassword && sudo nvidia-docker run -d -p 80:8888 $image"
 sudo docker login $server -u $regUsername -p $regPassword && sudo nvidia-docker run -d -p 80:8888 $image
