39e552d44c8261c39b6cfe7cd29d74c55e2e8819
William Buchwalter <wbuchwalter@gmail.com>
2018-03-27 16:03:27 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ image=$2
 regUsername=$3
 regPassword=$4
 
-echo "DEBUG: sudo docker login $server -u $regUsername -p $regPassword && sudo nvidia-docker run -d -p 80:8888 sherbrooke.azurecr.io/textworld-techfest:v3"
+echo "DEBUG: sudo docker login $server -u $regUsername -p $regPassword && sudo nvidia-docker run -d -p 80:8888 sherbrooke.azurecr.io/textworld-techfest:v4"
 n=0
 until [ $n -ge 5 ]
 do
-  sudo docker login $server -u $regUsername -p $regPassword && sudo nvidia-docker run -d -p 80:8888 sherbrooke.azurecr.io/textworld-techfest:v3 && break 
+  sudo docker login $server -u $regUsername -p $regPassword && sudo nvidia-docker run -d -p 80:8888 sherbrooke.azurecr.io/textworld-techfest:v4 && break 
   echo "Failed to start the docker image. Will retry in 15 seconds"
   n=$[$n+1]
   sleep 15
